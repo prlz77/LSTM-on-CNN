8bea92e6c74c8b42cbacda694884425fe1bf5ecd
Pau <pau.rodri1@gmail.com>
2016-06-08 12:32:08 +0200

    prepared gen_outputs script

---
+++ binaryproto2npy.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+""" Converts binaryproto file to npy. """
+
+__author__ = "Pau Rodríguez López, ISELAB, CVC-UAB"
+__email__ = "pau.rodri1@gmail.com"
+
+import os
+import sys
+from config import CAFFE_PATH
+sys.path.insert(0, os.path.join(CAFFE_PATH, 'python'))
+sys.path.insert(1, os.path.join(CAFFE_PATH, 'tools/extra'))
+import caffe
+import caffe.proto.caffe_pb2 as caffe_pb2
+import numpy as np
+
+def proto2npy(path):
+    with open(path) as infile:
+        blob = caffe_pb2.BlobProto().FromString(infile.read())
+
+    return caffe.io.blobproto_to_array(blob)
+
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    blob_array = proto2npy(path)
+    if len(sys.argv) < 3:
+        sys.argv.append(sys.argv[1] + '.npy')
+    np.save(sys.argv[2], blob_array[0])
+
+
--- gen_outputs.py
+++ gen_outputs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """ Generates the outputs of an arbitrary CNN layer. """
 
 __author__ = "Pau Rodríguez López, ISELAB, CVC-UAB"
@@ -17,30 +17,38 @@ import numpy as np
 
 
 # CMD Options
-parser = argparse.ArgumentParser(description='Generates the outputs of an arbitrary CNN layer.')
+parser = argparse.ArgumentParser(description="""Generates the outputs of an arbitrary CNN layer
+accepts either a LMDB dataset or a listfile of images.""")
 parser.add_argument('model', type=str, help='The model deploy file.')
 parser.add_argument('weights', type=str, help='The model weights file.')
 parser.add_argument('layer', type=str, nargs='+', help='The target layer(s).')
 parser.add_argument('--output', type=str, help='The output file.', default='output.h5')
 parser.add_argument('--flist', nargs=2, type=str, help='The base folder and the file list of the images.', default=None)
 parser.add_argument('--dataset', type=str, help='The lmdb dataset.', default=None)
-parser.add_argument('--sort', type=bool, default=False)
-parser.add_argument('--mean', type=int, nargs=3, default=None, help='Pixel mean (3 values)')
+parser.add_argument('--sort', action='store_true', description="Whether images should be sorted")
+parser.add_argument('--mean', type=int, nargs=3, default=None, help='Pixel mean (3 bgr values)')
 parser.add_argument('--mean_file', type=str, default=None, help='Per-pixel mean in bgr')
 parser.add_argument('--scale', type=int, default=None, help='Scale value.')
-parser.add_argument('--swap', type=bool, default=True, help='BGR <-> RGB.')
+parser.add_argument('--swap', action='store_true', help='BGR <-> RGB. If using --flist, images are loaded in BGR by default.')
 parser.add_argument('--cpuonly', action='store_false', description='CPU-Only flag.')
+parser.add_argument('--sequential', action='store_true', description="""Should receive list file with:
+frame_path<space>label<space>seq_number<return>""")
 args = parser.parse_args()
 
+# CPU ONLY
 if not args.cpuonly:
     caffe.set_mode_gpu()
 
+# Read Deploy + Weights file
 net = caffe.Net(args.model, args.weights,
                 caffe.TEST)
 
 # input preprocessing: 'data' is the name of the input blob == net.inputs[0]
 transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+# images are assumed to be in format hxwxc
 transformer.set_transpose('data', (2,0,1))
+
+# get mean
 if args.mean != None:
     transformer.set_mean('data', np.array(args.mean))#np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1)) # mean pixel
 elif args.mean_file != None:
@@ -56,21 +64,13 @@ if args.scale:
 if args.swap:
     transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
 
-LMDB=args.dataset
-
-from lmdb_visualizer import get_lmdb_iterator
-
-
-
-import Image
-import StringIO
-import caffe.proto.caffe_pb2
-
-
+# Auxiliar variables
 labels = []
 outputs = []
 seq_numbers_set = []
 seq_nums = []
+
+# We received a txt listfile, not lmdb
 if args.flist != None:
     from cv2 import imread
     with open(args.flist[1], 'r') as infile:
@@ -87,13 +87,14 @@ if args.flist != None:
         img = imread(os.path.join(args.flist[0], spline[0]))
         net.blobs['data'].data[...] = transformer.preprocess('data', img)
         out = net.forward()
-        ## REMOVE
-        seq_number = spline[0].split("/")[3]
+        # Get sequence numbers
+        seq_number = spline[-1]
         if seq_number not in seq_numbers_set:
             seq_numbers_set.append(seq_number)
             print 'Current seeq number: ', seq_numbers_set.index(seq_number)
         seq_nums.append(seq_numbers_set.index(seq_number))
-        ## END REMOVE
+
+        # for debugging
         labels.append(float(spline[1]))
 
         for index, layer in enumerate(args.layer):
@@ -101,8 +102,9 @@ if args.flist != None:
             outputs[index]['labels'][i,...] = labels[-1]
             outputs[index]['seq_number'][i,...] = seq_nums[-1]
         if i % 1000 == 0:
-            print "Processing image ", i
+            print "Processing image ", i, " of ", len(flist)
 elif args.dataset != None:
+    LMDB = args.dataset
     iterator = get_lmdb_iterator(LMDB)
 
     for index, it in enumerate(iterator):
@@ -121,13 +123,5 @@ elif args.dataset != None:
 else:
     raise Exception('need a dataset')
 
-
 for index, layer in enumerate(args.layer):
     outputs[index].close()
-
-# f = h5py.File(args.output, 'w')
-#
-# f.create_dataset('outputs', data=np.asarray(outputs), compression="gzip")
-# f.create_dataset('labels', data=np.asarray(labels))
-# f.create_dataset('seq_number', data=np.asarray(seq_nums))
-# f.close()
