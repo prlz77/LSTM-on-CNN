9bfaf9e1aac2f4aab861092112853e6998398b5d
Pau Rodriguez <prlz77@users.noreply.github.com>
2017-03-23 10:40:32 +0100

    Update binaryproto2npy.py

--- binaryproto2npy.py
+++ binaryproto2npy.py
@@ -14,18 +14,16 @@ import caffe.proto.caffe_pb2 as caffe_pb2
 import numpy as np
 
 def proto2npy(path):
+""" Loads binaryproto and returns numpy array """
     with open(path) as infile:
         blob = caffe_pb2.BlobProto().FromString(infile.read())
 
     return caffe.io.blobproto_to_array(blob)
 
-
-
+# Main loop
 if __name__ == '__main__':
     path = sys.argv[1]
     blob_array = proto2npy(path)
     if len(sys.argv) < 3:
         sys.argv.append(sys.argv[1] + '.npy')
     np.save(sys.argv[2], blob_array[0])
-
-
